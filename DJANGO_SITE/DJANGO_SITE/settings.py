4f2348a17353b3201e0b7e4b832b85250071a5d7
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-08-13 22:13:57 +0545

    conf. for media files

--- DJANGO_SITE/DJANGO_SITE/settings.py
+++ DJANGO_SITE/DJANGO_SITE/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/files/'
+MEDIA_ROOT= BASE_DIR / 'uploads'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
