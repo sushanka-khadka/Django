ba3f7c47c6e4f1ceffbc63b540397a2ad53c525c
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-08-13 22:43:58 +0545

    adding ModelAdmin

--- DJANGO_SITE/meetups/admin.py
+++ DJANGO_SITE/meetups/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Meetup
 
 # Register your models here.
-admin.site.register(Meetup)
+class MeetupAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'location']
+    list_filter = ['title', 'location']    
+
+admin.site.register(Meetup, MeetupAdmin)
 
