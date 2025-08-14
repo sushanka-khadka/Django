2de47cd7c89eb128d1ed379ef3dd578db62607a4
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-08-14 20:16:26 +0545

    switching from ModelForm -> Form

--- DJANGO_SITE/meetups/forms.py
+++ DJANGO_SITE/meetups/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from .models import Participant
 
-class RegistrationForm(forms.ModelForm):
-    class Meta:
-        model= Participant
-        fields = ['email']
+class RegistrationForm(forms.Form):     # has no relation with model so will not force unique email, as get_or_create method is used earlier, which will only create if it didn't exist
+    email = forms.EmailField()
+
+
+# class RegistrationForm(forms.ModelForm):    # force form for unique email validation 
+#     class Meta:
+#         model= Participant
+#         fields = ['email']
