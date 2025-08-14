42d6fb793e6eafecfa6703cc72e7aee26477b1a7
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-08-14 22:13:04 +0545

    RedirectView

--- DJANGO_SITE/meetups/urls.py
+++ DJANGO_SITE/meetups/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.index),  # domain/
-    path('meetups/<slug:meetup_slug>', views.meetup_details, name='meetup-detail'),    # doimain/meetups
-    path('meetups/<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registration'),   
+    path('', views.index),  # domain/    
+    path('<slug:meetup_slug>', views.meetup_details, name='meetup-detail'),    # doimain/meetups
+    path('<slug:meetup_slug>/success', views.confirm_registration, name='confirm-registration'),   
 ]
