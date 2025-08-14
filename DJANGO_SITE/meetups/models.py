3d5904583b844069d6647d8707115be408c760a8
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-08-14 17:24:18 +0545

    many to many relations

--- DJANGO_SITE/meetups/models.py
+++ DJANGO_SITE/meetups/models.py
@@ -1,12 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    name= models.CharField(max_length=200)
+    address= models.CharField(max_length=300)
+
+    def __str__(self):
+        return f"{self.name} ({self.address})"
+    
+class Participant(models.Model):
+    email= models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)    # default: null=False, blank=False
-    location = models.CharField(max_length=100, default='yet do decide')
+    organizer_email = models.EmailField()
+    date= models.DateField()
     slug= models.SlugField(unique=True)
     description= models.TextField()
     featured_image= models.ImageField(upload_to='images')
+    location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    participants= models.ManyToManyField(Participant, blank=True)
+    # participants= models.ManyToManyField(Participant, null=True, blank=True)
+    # meetups.Meetup.participants: (fields.W340) null has no effect on ManyToManyField. (warning)
     
     def __str__(self):
         return self.title
