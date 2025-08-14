e7a56e69f56393a7fa468474bf3f2a7bf40e8cd9
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-08-14 20:16:56 +0545

    get_or_create method

--- DJANGO_SITE/meetups/views.py
+++ DJANGO_SITE/meetups/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Meetup
+from .models import Meetup, Participant
 from .forms import RegistrationForm
 # Create your views here.
 
@@ -18,15 +18,18 @@ def meetup_details(request, meetup_slug):
         else:
             registration_form = RegistrationForm(request.POST)
             if registration_form.is_valid():
-                participant =registration_form.save()   # saving form to model Participant
-                selected_meetup.participants.add(participant)   # adding to many to many field
+                user_email = registration_form.cleaned_data['email']
+                participant, was_created= Participant.objects.get_or_create(email= user_email)
+                # participant =registration_form.save()   # saving form to model Participant
+                selected_meetup.participants.add(participant)   # adding to many to many field                
                 return redirect('confirm-registration')            
         return render(request, 'meetups/meetup-detail.html', {
             'meetup_found': True,
             'meetup_item': selected_meetup,
             'form': registration_form
         });
-    except:
+    except Exception as ex:
+        print(ex)
         return render(request, 'meetups/meetup-detail.html', {
             'meetup_found': False
         });
