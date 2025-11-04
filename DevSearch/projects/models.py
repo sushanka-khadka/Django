8ff394445f51dcba7639a04845e0da2eaaaa5464
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-11-04 21:32:08 +0545

    models added

---
+++ DevSearch/projects/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+import uuid
+
+# Create your models here.
+class Project(models.Model):
+    # owner
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    feature_image = models.ImageField(null=True, blank=True, default="projects/default.jpg")
+    source_link = models.URLField(max_length=200, null=True, blank=True)
+    demo_link = models.URLField(max_length=200, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', blank=True)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.title)
+
+
+class Review(models.Model):
+    Vote_Type = (
+        ('up', 'Up Vote'),
+        ('down', 'Down Vote'),
+    )
+    # owner 
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    body = models.TextField(null=True, blank=True)
+    value = models.CharField(choices=Vote_Type)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.value)
+    
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200)    
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+
+    def __str__(self):
+        return str(self.name)
