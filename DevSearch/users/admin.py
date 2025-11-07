fe791cdccc2efac8767e652c73be6edf1b4c7ead
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-11-07 16:51:52 +0545

    ModelAdmin conf

--- DevSearch/users/admin.py
+++ DevSearch/users/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from .models import Profile, Skill
 
+
 # Register your models here.
-admin.site.register(Profile)
+# admin.site.register(Profile)
 admin.site.register(Skill)
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['user', 'first_name', 'last_name', 'email', 'username', 'location']
+
+
+admin.site.register(Profile, ProfileAdmin)
