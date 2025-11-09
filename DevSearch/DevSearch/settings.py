cd9e529450dcb5afb0d5073bd8ee33b4a8c66737
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-11-09 17:57:36 +0545

    environment variables

--- DevSearch/DevSearch/settings.py
+++ DevSearch/DevSearch/settings.py
@@ -19,13 +19,22 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
+
+# environment variables
+import os
+from dotenv import load_dotenv
+
+ # Load environment variables from .env file
+load_dotenv()
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i24xw(_l_9*c&&7@@19!qfxu=z6cr0gd+x8#r8zit_fm+htf94'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -115,13 +124,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# environment variables
-import os
-from dotenv import load_dotenv
-
- # Load environment variables from .env file
-load_dotenv()
-
 # Email Configuration
 # EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'     # for development purpose only (prints emails to console)
 
@@ -138,6 +140,8 @@ EMAIL_HOST_PASSWORD = os.getenv('host_email_password')
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # for production (all static files will be collected here)
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
