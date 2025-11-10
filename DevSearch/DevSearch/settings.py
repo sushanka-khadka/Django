897c2d897d339debefae69c62fee68cce3b19b2b
sushanka-khadka <sushanka-khadka@users.noreply.github.com>
2025-11-10 15:29:01 +0545

    docker & containerization

--- DevSearch/DevSearch/settings.py
+++ DevSearch/DevSearch/settings.py
@@ -58,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # whitenoise middleware for serving static files in production
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'DevSearch.urls'
@@ -153,3 +155,7 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'     # for production (serving static files efficiently)
+
